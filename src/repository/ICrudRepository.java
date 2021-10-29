dde296b518210e9c81c6fbe41c431e1794825432
IoanaTincu <91495477+IoanaTincu@users.noreply.github.com>
2021-10-29 13:38:06 +0300

    ICrudRepository

---
+++ src/repository/ICrudRepository.java
@@ -0,0 +1,42 @@
+package repository;
+
+/**
+ * CRUD operations repository interface
+ * @param <T> generic class
+ */
+public interface ICrudRepository<T> {
+
+    /**
+     *
+     * @param id -the id of the entity to be returned id must not be null
+     * @return the entity with the specified id or null - if there is no entity with the given id
+     */
+    T findOne(Long id);
+
+    /**
+     *
+     * @return all entities
+     */
+    Iterable<T> findAll();
+
+    /**
+     *
+     * @param entity entity must be not null
+     * @return null- if the given entity is saved otherwise returns the entity (id already exists)
+     */
+    T save(T entity);
+
+    /**
+     * removes the entity with the specified id
+     * @param id id must be not null
+     * @return the removed entity or null if there is no entity with the given id
+     */
+    T delete(Long id);
+
+    /**
+     *
+     * @param entity entity must not be null
+     * @return null - if the entity is updated, otherwise returns the entity - (e.g id does not exist).
+     */
+    T update(T entity);
+}
